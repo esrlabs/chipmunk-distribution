f008610e3a03fd8e5aaa77fe9cc7a4676a5e54e9
Sameer Srivastava <itsmesamster@gmail.com>
2023-10-24 19:34:34 +0200

    Add package creation scripts for chocolatey
    Add package creation, installation and uninstallation script for chocolatey.

---
+++ chocolatey_win/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop';
+#if( -not ( get-command Install-ChocolateyPackage -erroraction silentlycontinue ) ) {
+#    Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
+#}
+
+$packageName= 'chipmunk'
+$version	= '3.9.22'
+$zipname	= "chipmunk@$version-win64-portable"
+$toolsDir	= "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = "https://github.com/esrlabs/chipmunk/releases/download/$version/chipmunk@$version-win64-portable.tgz"
+$shortcut	= "$env:ChocolateyInstall\bin\chipmunk.exe"
+$DesktopPath= [Environment]::GetFolderPath("Desktop")
+$exe		= "$DesktopPath\chipmunk.lnk"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'chipmunk*'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip -FileFullPath "$toolsDir/$zipname.tar" -Destination $toolsDir
+Install-ChocolateyShortcut -ShortcutFilePath $exe -TargetPath $shortcut
---
+++ chocolatey_win/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$DesktopPath= [Environment]::GetFolderPath("Desktop")
+$exe		= "$DesktopPath\chipmunk.lnk"
+
+if (Test-Path $exe) {
+   Remove-Item $exe
+}
