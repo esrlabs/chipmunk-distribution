998410cea4ee4c9c407b41f2803349ffd1d6f890
Frincu <alexandru.frincu@accenture.com>
2023-11-22 16:43:20 +0200

    Test commit: pass release to untar script

--- apt/scripts/get_release.sh
+++ apt/scripts/get_release.sh
@@ -24,4 +24,10 @@ mkdir -p "$working_dir"
 # Store the release in the working_dir folder
 cd "$working_dir"
 wget $chipmunk_package_url
-ls -l
+
+# replace hardcoded renaming of release
+mv chipmunk@3.10.3-linux-portable.tgz chipmunk@3.10.3-linux-portable.tar.gz
+
+# test message to print location
+pwd
+
