81bcaccf07080319f4d41607c94ca66013d4ac04
Frincu <alexandru.frincu@accenture.com>
2023-11-22 10:40:24 +0200

    Add step for retrieving release from chipmunk repo

---
+++ apt/scripts/get_release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eux
+
+url_chipmunk='https://github.com/esrlabs/chipmunk/releases/latest'
+response=$(curl -s -L -I $url_chipmunk)
+realTagUrl=$(echo "$response" | grep -i "location" | awk -F' ' '{print $2}')
+version=$(echo "$realTagUrl" | awk -F'/' '{print $NF}' | sed 's/v//' | sed 's/\r$//')
+
+echo "Packaging chipmunk version - '$version'"
+chipmunk_package_url="https://github.com/esrlabs/chipmunk/releases/download/$version/chipmunk@$version-linux-portable.tgz"
+
+# Folder structure
+output_dir=$(pwd)
+source_dir=$(dirname "$0")
+working_dir=/tmp/chipmunk_work_dir
+
+echo "source_dir = '$source_dir'"
+
+# Create an empty working directory
+sudo rm -rf "$working_dir"
+mkdir -p "$working_dir"
+
+# Store the release in the working_dir folder
+cd "$working_dir"
+wget $chipmunk_package_url
+ls -l
