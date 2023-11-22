66812553a67fa3bf5564848eb800fc85ea4a0aa6
Sameer Srivastava <itsmesamster@gmail.com>
2023-11-22 05:06:31 +0100

    Refactor chocolatey code and updated token requirement

--- distribute_to_chocolatey.ps1
+++ distribute_to_chocolatey.ps1
@@ -4,28 +4,21 @@ param(
 
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
+$version = '1.0.0'
 $homeDir = (Resolve-Path "$PSScriptRoot\.").Path
-$version = "3.9.22"
 $chocoCommand = "choco"
-$choco_token = "a07d53e8-fba5-4b78-906d-cee91e9bcb7e"
+$choco_token = $secrets:CHOCO_TOKEN_M1
 $choco = @{
   homeDir = "$homeDir\chocolatey_win"
   nuspec = "$homeDir\chocolatey_win\chipmunk.nuspec"
   chocoScript = "$homeDir\chocolatey_win\tools\chocolateyinstall.ps1"
 }
-
-function UpdateChocoConfig {
-    param($chocoScriptPath, $chocoNuspecPath, $version)
-    $chocoScript = Get-Content $chocoScriptPath -Encoding UTF8 -Raw
-    $chocoScript | Set-Content $chocoScriptPath -Force -Encoding UTF8
     
-    $chocoNuspec = Get-Content $chocoNuspecPath -Encoding UTF8 -Raw
-    $chocoNuspec | Set-Content $chocoNuspecPath -Force -Encoding UTF8
-}
+$chocoNuspec = Get-Content $choco.nuspec -Encoding UTF8 -Raw
+$chocoNuspec = [Regex]::Replace($chocoNuspec, '(<version>[\d.]+<\/version>)', "<version>$version</version>")
+$chocoNuspec | Set-Content $choco.nuspec -Force -Encoding UTF8
 
 $nupkgName = "chipmunk.$version.nupkg"
-
-UpdateChocoConfig $choco.chocoScript $choco.nuspec $version
 
 if ($publish) {
   # create the nuspec package
@@ -33,8 +26,8 @@ if ($publish) {
 
   # if token is given, we will publish the package to Chocolatey here
   if ($choco_token) {
-    & $chocoCommand apiKey -k $choco_token -source https://push.chocolatey.org/
-    & $chocoCommand push $nupkgName -source https://push.chocolatey.org/ -dv
+    & $chocoCommand apiKey --key $choco_token --source https://push.chocolatey.org/
+    & $chocoCommand push $nupkgName --source https://push.chocolatey.org/ -dv
   } else {
     Write-Warning "Chocolatey token was not set. Publication skipped."
   }
