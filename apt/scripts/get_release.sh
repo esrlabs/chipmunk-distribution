bd25e9305f103c6d6ee5385085e461649aa5c122
Frincu <alexandru.frincu@accenture.com>
2023-11-24 17:12:36 +0200

    check location of untared chipmunk

--- apt/scripts/get_release.sh
+++ apt/scripts/get_release.sh
@@ -36,10 +36,6 @@ if [ -n "$file" ]; then
   # Move the file with the new extension
   mv "$file" "$filename.tar.gz"
   echo "TAR_FILE=$working_dir/$filename.tar.gz" >> $GITHUB_ENV
-
-  pwd
-  ls -l
-  
 else
   echo "File not found."
 fi
